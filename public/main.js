420cc3c8d1bd5cd3ae6268847e4d4f9adbc067bd
Maria M <prov21_1@hotmail.com>
2021-07-02 15:57:36 -0700

    issue with json parse in mainjs

--- public/main.js
+++ public/main.js
@@ -3,28 +3,29 @@ const form = document.getElementById("vote-form");
 //Form submit event
 form.addEventListener("submit", (e) => {
   console.log($('input[name="restaurant"]:checked').serialize());
-  const choice = document.querySelector('input[name="restaurant"]:checked')
+  const choice = document.querySelectorAll('input[name="restaurant"]:checked')
     .value;
   const data = { restaurant: choice };
 
-  fetch("http://localhost:3000", {
+  fetch("http://localhost:3000/poll/", {
     method: "post",
     //body: JSON.stringify(data),
-    //body: JSON.parse(data),
+    body: JSON.parse(data),
+    //    body: JSON.parse(data.trim()),
     //body: data.json(),
-    body: data,
+    //body: data,
     headers: new Headers({
       "Content-Type": "application/json",
     }),
   })
-    //.then((res) => res.json())
+    .then((res) => res.json())
     .then((data) => console.log(data))
     .catch((err) => console.log(err));
 
   e.preventDefault();
 
   //v3 13m get request
-  fetch("http://localhost:3000/")
+  fetch("http://localhost:3000")
     .then((res) => res.json())
     .then((data) => console.log(data));
   //.catch((err) => console.log(err));
